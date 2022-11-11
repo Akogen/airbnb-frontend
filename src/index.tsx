ec8c8f42a6f3c0b0c53adacc14ecc3d18957541f
Jason Jung <akogenweb@gmail.com>
2022-11-11 13:09:49 -0800

    Route and Dark Mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,18 @@
-import { ColorModeScript } from "@chakra-ui/react";
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import { App } from "./App";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import theme from "./Theme";
 
-import * as serviceWorker from "./serviceWorker";
-
-const container = document.getElementById("root");
-if (!container) throw new Error("Failed to find the root element");
-const root = ReactDOM.createRoot(container);
-
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    <ColorModeScript />
-    <App />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </React.StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister();
